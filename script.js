f71fcb96cd6ba6f8195c15168044a5d0bd643098
ZPT\Lnk2 <72851896+MishaProhorenko@users.noreply.github.com>
2021-05-21 09:59:50 +0300

    123

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
 'use strict';
 
+class Honda {
+	constructor(modelName, color) {
+		this.modelName = modelName;
+		this.color = color;
+		this.isDisabled = false;
+	}
 
+	// static checkColor(car) {
+	// 	if (car.color === 'blue') return true
+	// 	return false;
+	// }
+
+	// static checkEngineVolume(car) {
+	// 	if (car.engineVolume > 2.4) return true
+	// 	return false;
+	// }
+
+	// checkActiveCar() {
+	// 	if (this.isDisabled) {
+	// 		console.log('wrrr');
+	// 	} else {
+	// 		console.log('----');
+	// 	}
+	// }
+
+};
+
+class Acura extends Honda {
+	constructor(modelName, color, engineVolume) {
+		super(modelName, color)
+		this.engineVolume = engineVolume;
+	}
+};
+
+class Audi extends Acura {
+	constructor(modelName, color, engineVolume, bodyType) {
+		super(modelName, color, engineVolume)
+		this.bodyType = bodyType;
+	}
+};
+
+class Opel extends Audi {
+	constructor(modelName, color, engineVolume, bodyType) {
+		super(modelName, color, engineVolume, bodyType)		
+	}
+};
+
+let hondaCar = new Honda('Accord', 'blue');
+let acuraCar = new Acura('TSX2', 'black', 2.4);
+let audiCar = new Audi('A6', 'silver', 2.5, 'sedan');
+let opelCar = new Opel('Zafira', 'gray', 2.2, 'minivan');
+
+console.log(hondaCar)
+console.log(acuraCar)
+console.log(audiCar)
+console.log(opelCar)
---
+++ script3.0.js
@@ -0,0 +1,96 @@
+'use strict';
+
+
+// function ConstFunc(value) {
+//     this.value = value;
+// };
+
+// const obj = new ConstFunc(42)
+
+
+
+class Robot {
+    constructor(modelName) {
+        this.modelName = modelName;
+        this.isDisabled = false;
+    }
+
+    makeSomeNoise() {
+        if (this.isDisabled) {
+            console.log('We weee')
+        } else {
+            console.log('.....')
+        }
+    }
+
+    toggleIsDisable() {
+        this.isDisabled = !this.isDisabled;
+        console.log(this.isDisabled);
+    }
+};
+
+
+class VacuumCleanerBot extends Robot {
+
+    makeRoomClear(room) {
+        room.isCleaned = true;
+
+        return room;
+    }
+    makeSomeNoise() {
+        super.makeSomeNoise();
+    }
+
+}
+
+
+class UberStaubsauger extends VacuumCleanerBot {
+    constructor(modelName, power) {
+        super(modelName)
+        this.date = new Date();
+        this.power = power;
+    }
+
+    makeFlatsClear(arrayOfRooms) {
+        arrayOfRooms.forEach(room => {
+            room.isCleaned = true;
+        });
+        return arrayOfRooms;
+    }
+
+}
+
+
+
+const verter = new Robot('Verter');
+const vacuum = new VacuumCleanerBot('Xiaomi', 9000);
+const hall = {
+    name: 'hall',
+    isCleaned: false,
+};
+const hans = new UberStaubsauger('Hans', 10000);
+const flat = [{
+        name: 'hall',
+        isCleaned: false,
+    },
+    {
+        name: 'kitchen',
+        isCleaned: false,
+    },
+    {
+        name: 'chiledroom',
+        isCleaned: false,
+    }
+];
+
+
+
+
+console.log(verter);
+
+console.log(vacuum);
+vacuum.toggleIsDisable();
+vacuum.toggleIsDisable();
+console.log(vacuum.makeSomeNoise());
+
+console.log(hans);
